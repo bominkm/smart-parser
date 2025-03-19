d7a916ec143b93cacdc9cc65c73e4d7e842f35c6
bominkm <kimbompro@gmail.com>
2025-03-19 20:23:49 +0900

    feat: Add transform function

--- parser/convertor.py
+++ parser/convertor.py
@@ -1,49 +1,22 @@
 import os
-from openai import OpenAI
+import fitz
 
-def inference_vlm(prompt: str, image_data: str) -> str:
+def convert_pdf_to_image(pdf_path: str, img_path: str, img_format: str = "jpg"):
     """
     Args:
-        prompt (str): vlm 프롬프트
-        image_data (str): Base64 인코딩된 이미지
-
-    Returns:
-        str: 변환된 문서 컨텐츠   
+        pdf_path (str): 변환할 PDF 경로
+        img_path (str): 변환된 이미지 경로
     """
-    client = OpenAI(
-        base_url="https://openrouter.ai/api/v1",
-        api_key="<OPENROUTER_API_KEY>",
-    )
+    os.makedirs(img_path, exist_ok=True)
 
-    message = {
-        {"role": "user",
-        "content": [
-            {
-               "type": "text", 
-               "text": prompt
-            },
-            {
-               "type": "image_url", 
-               'image_url': {"url": f"data:image/jpeg;base63,{image_data}"}}
-        ]}
-    }
+    doc = fitz.open(pdf_path)
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        pix = page.get_pixmap()
 
-    completion = client.chat.completions.create(
-        model="qwen/qwen-2.5-vl-7b-instruct",
-        messages=message
-    )
+        image_filename = f"page_{page_num + 1}.{img_format}"
+        image_path = os.path.join(img_path, image_filename)
 
-    return completion.choices[0].message.content
+        pix.save(image_path)
 
-
-def save_to_md(content: str, md_path: str):
-    """
-    Args:
-        content (str): 변환된 문서 컨텐츠
-        md_path (str): 마크다운 경로
-    """
-
-    os.mkdir(md_path)
-
-    with open(md_path, 'w', encoding='utf-8') as f:
-        f.write(content)
+    doc.close()
